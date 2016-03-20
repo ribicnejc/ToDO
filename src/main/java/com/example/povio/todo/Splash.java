838c2cbecd94d089b70e90ba12040263e3a0cf02
ribicnejc <nejc.ribic95@gmail.com>
2016-03-20 18:01:47 +0100

    Updating Splash

--- src/main/java/com/example/povio/todo/Splash.java
+++ src/main/java/com/example/povio/todo/Splash.java
@@ -3,9 +3,10 @@ package com.example.povio.todo;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 public class Splash extends AppCompatActivity {
-    protected int _splashTime = 3000;
+    protected int _splashTime = 1500;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -19,6 +20,7 @@ public class Splash extends AppCompatActivity {
                     try {
                         wait(_splashTime);
                     } catch (InterruptedException e) {
+                        Log.d("Error", "error splash");
                     } finally {
                         finish();
                         Intent intent = new Intent(getBaseContext(), ScrollingActivity.class);
